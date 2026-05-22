e099b2ebb24627599bb81df9c7d851b5d5cc7144
dzakirozaan19-oss <dzakirozaan19@gmail.com>
2026-05-22 16:40:25 +0700

    Add offline cache

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
+const CACHE_NAME = 'webku-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
 self.addEventListener('install', e => {
-  console.log('Service Worker installed');
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+self.addEventListener('activate', e => {
+  clients.claim();
+});
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
 });
